df7f301b52836386f591c4af999d15020a9a966a
Daniel Sanabria <danielsanabriaalbert@gmail.com>
2025-08-18 08:00:18 +0000

    https://iili.io/FbANuA7.jpg Podemos añadir esto como favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="es" className="dark">
       <head>
+        <link rel="icon" href="https://iili.io/FbANuA7.jpg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Playfair+Display:wght@700;900&display=swap" rel="stylesheet" />
